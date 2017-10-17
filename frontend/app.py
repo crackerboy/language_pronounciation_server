508fcdaafcd5b7bab680b3d68f94ec90f28e4c5a
Josue Caraballo <josue.caraballo@mavs.uta.edu>
2017-10-17 09:01:00 -0500

    Finished testing POST communication, seems to work well from test. Will continue with creating difference computation function.

--- frontend/app.py
+++ frontend/app.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, request
+from flask_restful import Resource, Api
 from sql_classes import Base, Sample
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import werkzeug
-import pdb
 
 _MYSQL_URL_ = 'mysql://root:senior-design-2@localhost:3306/teamc'
 
 app = Flask(__name__)
 api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('word', required=True, help="Need a word to compare against...",\
-                    type=str, location='data')
-parser.add_argument('audio_file', required=True, help="Need audio input...",\
-                    type=werkzeug.datastructures.FileStorage, location='files')
 
 engine = create_engine(_MYSQL_URL_)
 Base.metadata.create_all(engine)
@@ -26,16 +19,15 @@ session = DBSession()
 class CompareAudio(Resource):
     def post(self):
         # Load samples and input sample into memory
-        pdb.set_trace()
-        args = parser.parse_args()
-        file_paths = session.query(Sample.file_path).filter(Sample.word == args['word']).all()
+        word, input_audio = (request.form['word'], request.files['audio_file'])
+        file_paths = session.query(Sample.file_path).filter(Sample.word == word).all()
         files = {file_path: open(file_path,'r') for file_path in file_paths} #be sure to close files
 
         # Magic
         score = 0
         for path, file_obj in files.items():
             file_obj.close()
-        return "{word:{},score:{}}".format(args['word'],score)
+        return "{word:{},score:{}}".format(word,score)
 
 api.add_resource(CompareAudio, '/')
 if __name__=='__main__':
--- frontend/test_me/test_app.py
+++ frontend/test_me/test_app.py
@@ -3,9 +3,8 @@ import requests
 import pdb
 
 url = 'http://localhost:1337/'
-headers = {'Content-type':'multipart/form-data'}
 files = {'audio_file':open('us1.mp3','rb')}
 data = {'word':'us'}
 pdb.set_trace()
-r = requests.post(url, files=files, data=data, headers=headers)
+r = requests.post(url, files=files, data=data)
 print(r.content)
